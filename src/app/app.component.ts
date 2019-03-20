da68ba16946daff1b8d9e99bb16ef903496b820c
Martin Gahmberg <martin.gahmberg@conzignus.se>
2019-03-20 20:27:47 +0100

    mk
    mk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,10 @@ export class AppComponent implements OnInit {
                       {'title' : 'img3', 'src': 'Martin_3.png'},
                       {'title' : 'img4', 'src': 'Martin_4.png'},
                       {'title' : 'img5', 'src': 'Martin_5.png'},
-                      {'title' : 'img6', 'src': 'Martin_6.png'}];
+                      {'title' : 'img6', 'src': 'Martin_6.png'},
+                      {'title' : 'img7', 'src': 'Martin_7.png'},
+                      {'title' : 'img8', 'src': 'Martin_8.png'},
+                      {'title' : 'img9', 'src': 'Martin_9.png'}];
 
     ngOnInit() {
         setTimeout(() => { externalJS.initCarousel(); }, 500);
