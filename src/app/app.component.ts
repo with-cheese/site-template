140c7c528b16423a8fa3c52f7580e80860766d86
Martin Gahmberg <martin.gahmberg@conzignus.se>
2019-03-31 12:16:59 +0200

    First commit
    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,15 @@ declare var externalJS: any;
 
 export class AppComponent implements OnInit {
     public title = 'site-template';
-    public images = [ {'title' : 'img1', 'src': 'Martin_1.png'},
-                      {'title' : 'img2', 'src': 'Martin_2.png'},
-                      {'title' : 'img3', 'src': 'Martin_3.png'},
-                      {'title' : 'img4', 'src': 'Martin_4.png'},
-                      {'title' : 'img5', 'src': 'Martin_5.png'},
-                      {'title' : 'img6', 'src': 'Martin_6.png'}];
+    public images = [ {'title' : 'img1', 'src': 'Nina_1.png'},
+                      {'title' : 'img2', 'src': 'Nina_2.png'},
+                      {'title' : 'img3', 'src': 'Nina_3.png'},
+                      {'title' : 'img4', 'src': 'Nina_4.png'},
+                      {'title' : 'img5', 'src': 'Nina_5.png'},
+                      {'title' : 'img6', 'src': 'Nina_6.png'},
+                      {'title' : 'img7', 'src': 'Nina_7.png'},
+                      {'title' : 'img8', 'src': 'Nina_8.png'},
+                      {'title' : 'img9', 'src': 'Nina_9.png'}];
 
     ngOnInit() {
         setTimeout(() => { externalJS.initCarousel(); }, 500);
