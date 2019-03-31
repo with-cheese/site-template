b51c56dede0766751deb22f5f84675352b354ac9
Martin Gahmberg <martin.gahmberg@conzignus.se>
2019-03-31 13:01:01 +0200

    First commit
    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,15 @@ declare var externalJS: any;
 
 export class AppComponent implements OnInit {
     public title = 'site-template';
-    public images = [ {'title' : 'img1', 'src': 'Nina_1.png'},
-                      {'title' : 'img2', 'src': 'Nina_2.png'},
-                      {'title' : 'img3', 'src': 'Nina_3.png'},
-                      {'title' : 'img4', 'src': 'Nina_4.png'},
-                      {'title' : 'img5', 'src': 'Nina_5.png'},
-                      {'title' : 'img6', 'src': 'Nina_6.png'},
-                      {'title' : 'img7', 'src': 'Nina_7.png'},
+    public images = [ {'title' : 'img1', 'src': 'Stenmark_1.png'},
+                      {'title' : 'img2', 'src': 'Stenmark_2.png'},
+                      {'title' : 'img3', 'src': 'Stenmark_3.png'},
+                      {'title' : 'img4', 'src': 'Stenmark_4.png'},
+                      {'title' : 'img5', 'src': 'Stenmark_5.png'},
+                      {'title' : 'img6', 'src': 'Stenmark_6.png'},
+                      {'title' : 'img7', 'src': 'Stenmark_7.png'},
                       {'title' : 'img8', 'src': 'Nina_8.png'},
-                      {'title' : 'img9', 'src': 'Nina_9.png'}];
+                      {'title' : 'img9', 'src': 'Stenmark_9.png'}];
 
     ngOnInit() {
         setTimeout(() => { externalJS.initCarousel(); }, 500);
